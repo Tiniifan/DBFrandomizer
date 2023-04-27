7e8d8b9800b28f4fea805774d9acb21da1655922
Tinifan <30804632+Tiniifan@users.noreply.github.com>
2023-04-27 21:14:45 +0200

    Add criptography to RandomNumber.cs
    Added criptography to generate better random number

--- DBFrandomizer/Randomizer/RandomNumber.cs
+++ DBFrandomizer/Randomizer/RandomNumber.cs
@@ -1,72 +1,103 @@
 ﻿using System;
 using System.Linq;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 
 namespace DBFrandomizer.Randomizer
 {
-    public class RandomNumber : Random
+    public class RandomNumber
     {
-        public readonly int Seed;
+        private readonly RNGCryptoServiceProvider _rngCrypto;
 
         public RandomNumber()
         {
-            Seed = new Random().Next();
+            _rngCrypto = new RNGCryptoServiceProvider();
         }
 
-        public RandomNumber(int seed) : base(seed)
+        public int Next(int minValue, int maxValue)
         {
-            Seed = seed;
-        }
+            if (minValue >= maxValue)
+                throw new ArgumentOutOfRangeException(nameof(minValue), "minValue must be less than maxValue");
 
-        public int Next(int min, int max, int excludeIndex)
-        {
-            int randomNumber = Next(min, max);
+            byte[] randomNumber = new byte[4];
+            int value;
 
-            while (randomNumber == excludeIndex)
+            do
             {
-                randomNumber = Next(min, max);
+                _rngCrypto.GetBytes(randomNumber);
+                value = System.BitConverter.ToInt32(randomNumber, 0);
+            } while (value < minValue || value >= maxValue);
+
+            return value;
+        }
+
+        public int Next(int maxValue)
+        {
+            return Next(0, maxValue);
+        }
+
+        public int Next()
+        {
+            return Next(int.MinValue, int.MaxValue);
+        }
+
+        public List<int> GetNumbers(int minValue, int maxValue, int count)
+        {
+            if (count > (maxValue - minValue))
+                throw new ArgumentException("count cannot be greater than the range between minValue and maxValue");
+
+            List<int> randomList = new List<int>();
+
+            for (int i = minValue; i < maxValue; i++)
+            {
+                randomList.Add(i);
             }
 
-            return randomNumber;
-        }
-
-        public List<int> GetNumbers(int min, int max, int count)
-        {
-            List<int> randomList = new List<int>(new int[count]);
-
-            for (int i = 0; i < randomList.Count; i++)
+            for (int i = 0; i < count; i++)
             {
-                int randomNumber = Next(min, max);
-
-                while (randomList.IndexOf(randomNumber) == -1)
-                {
-                    randomList[i] = randomNumber;
-                }
+                int randomIndex = Next(i, maxValue);
+                int temp = randomList[randomIndex];
+                randomList[randomIndex] = randomList[i];
+                randomList[i] = temp;
             }
 
-            return randomList;
+            return randomList.GetRange(0, count);
         }
 
-        public List<int> GetNumbers(int min, int max, int count, List<int> excludeIndex)
+        public List<int> GetNumbers(int minValue, int maxValue, int count, List<int> excludeList)
         {
-            List<int> randomList = new List<int>(new int[count]);
+            if (count > (maxValue - minValue) - excludeList.Count)
+                throw new ArgumentException("count cannot be greater than the range between minValue and maxValue, minus the number of excluded values");
 
-            for (int i = 0; i < randomList.Count; i++)
+            List<int> randomList = new List<int>();
+
+            for (int i = minValue; i < maxValue; i++)
             {
-                int randomNumber = Next(min, max);
-
-                while (randomList.IndexOf(randomNumber) == -1 && excludeIndex.IndexOf(randomNumber) == -1)
-                {
-                    randomList[i] = randomNumber;
-                }
+                if (!excludeList.Contains(i))
+                    randomList.Add(i);
             }
 
-            return randomList;
+            for (int i = 0; i < count; i++)
+            {
+                int randomIndex = Next(i, randomList.Count);
+                int temp = randomList[randomIndex];
+                randomList[randomIndex] = randomList[i];
+                randomList[i] = temp;
+            }
+
+            return randomList.GetRange(0, count);
         }
 
         public int Probability(int[] itemsProbability)
         {
             List<double> pool = itemsProbability.Select(x => x / 100.0).ToList();
+
+            double NextDouble()
+            {
+                byte[] randomNumber = new byte[4];
+                _rngCrypto.GetBytes(randomNumber);
+                return System.BitConverter.ToUInt32(randomNumber, 0) / (double)uint.MaxValue;
+            }
 
             double Next()
             {
